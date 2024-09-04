d61e82ca6a8687deb08f4c7c0043d3bd76bc43a9
Vyosim Devone <vyosim.mohammed.k@gmail.com>
2024-09-04 09:51:41 +0530

    adding frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Tags from './Tags';
+
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <Tags/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
